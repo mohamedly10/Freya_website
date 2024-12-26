d560c9297d84986903b4a5497175f159fa314fec
mohamedly10 <mohamedalhamali20@gmail.com>
2024-12-26 22:07:46 +0200

    Initial commit

--- src/compounts/Navbar/nav.js
+++ src/compounts/Navbar/nav.js
@@ -1,57 +1,118 @@
-import React, { Component } from "react";
+import React from "react";
 import "../Navbar/nav.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTwitter,faInstagram ,faBasketball} from "@fortawesome/free-brands-svg-icons";
-import facebookIcon from "../../assets/icon/facebook.png"
-import instaIcon from "../../assets/icon/instagram.png"
-import twitterIcon from "../../assets/icon/twitter.png"
-import ArrowIcon from "../../assets/icon/arrow.png"
-class NavBar extends Component {
-  render() {
-    return (
-      <>
-        <nav className="navbar">
-          <ul className="navbar-left">
-            <li className="logo"><a href="#">FREYA</a></li>
-            <div className="DropDown">
-              <li><a href="#">Home<img src={ArrowIcon}/></a></li>
-              <ul className="Drop_Down_content">
-              <li>about us</li>
-              <li>how are you</li>
-              <li>slider</li>
-              </ul>
-              </div>
-              <div className="DropDown">
-              <li><a href="#">page<img src={ArrowIcon}/></a></li>
-              <ul className="Drop_Down_content">
-              <li>about us</li>
-              <li>how are you</li>
-              <li>slider</li>
-              </ul>
-              </div>
-              <div className="DropDown">
-              <li><a href="#">Project<img src={ArrowIcon}/></a></li>
-              <ul className="Drop_Down_content">
-              <li>about us</li>
-              <li>how are you</li>
-              <li>slider</li>
-              </ul>
-              </div>
-            <li><a href="#">Blog<img src={ArrowIcon}/></a></li>
-            <li><a href="#">Contact<img src={ArrowIcon}/></a></li>
-            <li><a href="#">Elements<img src={ArrowIcon}/></a></li>
+import facebookIcon from "../../assets/icon/facebook.png";
+import instaIcon from "../../assets/icon/instagram.png";
+import twitterIcon from "../../assets/icon/twitter.png";
+import ArrowIcon from "../../assets/icon/arrow.png";
+
+// Navigation items data
+const navItems = [
+  { title: "Home", hasDropdown: true, dropdownItems: ["about us", "about us", "slider"] },
+  { title: "Page", hasDropdown: true, dropdownItems: ["about us", "about us", "about us"] },
+  { title: "Project", hasDropdown: true, dropdownItems: ["about us", "about us", "about us"] },
+  { title: "Blog", hasDropdown: false },
+  { title: "Contact", hasDropdown: false },
+  { title: "Elements", hasDropdown: false }
+];
+
+const socialIcons = [
+  { icon: facebookIcon, link: "#" },
+  { icon: twitterIcon, link: "#" },
+  { icon: instaIcon, link: "#" }
+];
+
+const NavDropdown = ({ title, dropdownItems }) => (
+  <div className="DropDown">
+    <li>
+      <a href="#">
+        {title}<img src={ArrowIcon} alt="arrow" />
+      </a>
+    </li>
+    <ul className="Drop_Down_content">
+      {dropdownItems.map((item, index) => (
+        <li key={index}><a href="#">{item}</a></li>
+      ))}
+    </ul>
+  </div>
+);
+
+const NavDropdownSmall = ({ title, dropdownItems }) => (
+  <div className="DropDown">
+    <li>
+      <a href="#">
+        {title}
+      </a>
+    </li>
+    <ul className="Drop_Down_content">
+      {dropdownItems.map((item, index) => (
+        <li key={index}><a href="#">{item}</a></li>
+      ))}
+    </ul>
+  </div>
+);
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-left">
+        <li className="logo">
+          <a href="#">FREYA</a>
+        </li>
+        
+        <div className="option">
+          {navItems.map((item, index) => (
+            item.hasDropdown ? (
+              <NavDropdown 
+                key={index}
+                title={item.title}
+                dropdownItems={item.dropdownItems}
+              />
+            ) : (
+              <li key={index}>
+                <a href="#">
+                  {item.title}<img src={ArrowIcon} alt="arrow" />
+                </a>
+              </li>
+            )
+          ))}
+        </div>
+      </ul>
+
+      <ul className="navbar-right">
+        <div className="socialMedia">
+          {socialIcons.map((social, index) => (
+            <div key={index}>
+              <a href={social.link}>
+                <img src={social.icon} alt="social icon" />
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <div className="DropDown_smallScreen">
+          <li>
+            <a href="#">
+              <img src={ArrowIcon} alt="menu" />
+            </a>
+          </li>
+          <ul className="Drop_Down_content_s">
+            {navItems.map((item, index) => (
+              item.hasDropdown?
+           <NavDropdownSmall 
+           className="the"
+           title={item.title}
+           dropdownItems={item.dropdownItems}
+           />
+              :
+              <li key={index}>
+              <a href="#">{item.title}</a>
+            </li>
+            ))}
           </ul>
-          <ul className="navbar-right">
-            {/* Proper usage of FontAwesomeIcon */}
-            <div> <a href="#"><img src={facebookIcon}/></a></div>
-        <div> <a href="#"><img src={twitterIcon}/></a></div>
-        <div> <a href="#"><img src={instaIcon}/></a></div>
-            
-          </ul>
-        </nav>
-      </>
-    );
-  }
-}
+        </div>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
--- src/compounts/Types of furniture/type_of_furniture.js
+++ src/compounts/Types of furniture/type_of_furniture.js
@@ -4,16 +4,33 @@ import '../Types of furniture/type_furniture.css'
 const furnitures = TypeOfFurniture.furniture;
 
 class Type_of_furniture extends Component {
+
+    state={add_remove_cart:"add to cart",isinCart:false};
+    addcart=()=>{
+  
+if(this.state.isinCart){
+        this.setState({add_remove_cart:"remove from cart",isinCart:true})
+
+}
+else{
+    this.setState({add_remove_cart:"remove from cart",isinCart:false})
+}
+        
+       // this.setState({add_remove_cart:"add to cart"})
+    }
     render() {
         return (
             <>
             <div className="container">
                 {furnitures.map((furniture) => (
-          
+                    furniture.isinCart=false,
+                    
                         <div class="card">
                             <img src={furniture.image} alt="Image 1" class="card-image" />
                             <h3 class="card-title">{furniture.title}</h3>
                             <p class="card-description">{furniture.description}</p>
+                            <div className="add_cart" onClick={this.addcart}><a href="">{this.state.add_remove_cart}</a></div>
+                            
                       
                     </div>
                        
